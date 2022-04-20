5c131a79ccd3f3549961d5905655df3fa231b791
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2022-04-20 20:57:50 +0900

    sad
    asd

--- CakeRush/Assets/Scripts/EntityBase.cs
+++ CakeRush/Assets/Scripts/EntityBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EntityBase : ObjectBase
 {
@@ -8,6 +9,8 @@ public class EntityBase : ObjectBase
     protected float spawnTime;
     protected float moveSpeed;
 
+    NavMeshAgent agent;
+    
     protected virtual void EntityInit()
     {
 
