0650f867c6db7f0af217329abf2b3267ab46088f
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2022-05-16 19:48:31 +0900

    RTS 스크립트 복원
     rts 스크립트 복원

---
+++ CakeRush/Assets/Scripts/RTS/BuildBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 모든 건물의 최상위 부모 클래스
+public class BuildBase : EntityBase
+{
+    public void CilckSelectBuild(BuildController newBuild)
+    {
+        
+    }
+
+    private void DeselectBuild(BuildController newBuild)
+    {
+        
+    }
+
+    private void SelectBuild(BuildController newBuild)
+    {
+        
+    }
+}
