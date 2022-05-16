4cf4fe06b04ddb0244faa203353cd5d670d6dd0d
2422june <ck66661254@gmail.com>
2022-05-16 21:03:25 +0900

    s
    s

---
+++ CakeRush/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    RTSContoller rtsController;
+    
+    Vector3 destPos;
+
+    void Lock()
+    {
+        
+    }
+
+    void PosToSelectedUnit()
+    {
+
+    }
+}
---
+++ CakeRush/Assets/Scripts/Manager/UiManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    Image infoImage;
+    Image mapImage;
+
+    Text playTimeText;
+    Text unitSizeText;
+    Text eventText;
+    Text curCost;
+
+    // If select entity, on UI
+    void SetUI()
+    {
+
+    }
+
+    // Event notice method. use Fade in/out
+    void Notice()
+    {
+
+    }
+}
