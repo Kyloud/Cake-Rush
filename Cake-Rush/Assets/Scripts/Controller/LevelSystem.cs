ac5c111ae16685e67128d1dcb37a36f3e8fadb72
2422june <ck66661254@gmail.com>
2022-06-19 23:59:56 +0900

    통합 완료

---
+++ Cake-Rush/Assets/Scripts/Controller/LevelSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelSystem : MonoBehaviour
+{
+    private PlayerController playerController;
+    public float curExp { get; private set; }
+    public int curLevel { get; private set; }
+    public int skillPoint { get; private set; }
+
+    private const int maxLevel = 9;     //max index number
+    [SerializeField] private float []maxExp;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    public int SetLevel()
+    {
+
+        Debug.Log("Level Up");
+        skillPoint++;
+        return curLevel++;
+    }
+
+    private void LevelUp()
+    {
+        if(curExp >= maxExp[curLevel] && maxLevel > curLevel)
+        {
+            curExp = 0;
+            SetLevel();
+        }
+    }
+
+    public void GetExp(float returnExp)
+    {
+        curExp += returnExp;
+        LevelUp();
+    }
+
+    public void SkillLevelUp <T> (T skill) where T : SkillBase
+    {
+        if (skill.isSkillable == false)
+        {
+            skill.isSkillable = true;
+        }
+        else
+        {
+            if (skill.maxSkillLevel > skill.level)
+            {
+                skill.level++;
+            }
+        }
+
+        skillPoint--;
+        skill.skillStat[skill.level].currentCoolTime = skill.skillStat[skill.level - 1].currentCoolTime;
+        StartCoroutine(skill.skillStat[curLevel].CurrentCoolTime());
+        Debug.Log($"{skill.GetType()} skill level up {skill.level} / current skill point : {skillPoint}");
+    }
+}
